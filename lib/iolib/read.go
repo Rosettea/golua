903e783c9f431b3ca250086d53ba173fb62c2f52
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-04-02 17:11:26 -0400

    feat: support *all as a format for lua files

--- lib/iolib/read.go
+++ lib/iolib/read.go
@@ -55,7 +55,7 @@ func getFormatReader(fmt rt.Value) (reader formatReader, err error) {
 		switch s {
 		case "n", "*n":
 			reader = (*File).ReadNumber
-		case "a", "*a", "all":
+		case "a", "*a", "all", "*all":
 			reader = (*File).ReadAll
 		case "l", "*l":
 			reader = lineReader(false)
